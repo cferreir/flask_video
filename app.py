14e2702bcd725a5ef95141f610ccb2caa51f9384
Carlos Ferreira <carlos@cmfadvanced.com>
2018-12-18 11:14:10 -0800

    implement first stab at drone control

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 #!/usr/bin/env python
 from importlib import import_module
+import socket
 import os
-from flask import Flask, render_template, Response
+import sys
+from flask import Flask, render_template, Response, request
 
 # import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
+# if os.environ.get('CAMERA'):
+#    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+# else:
+#    from camera import Camera
+
+from camera_opencv import Camera  # Hard code using web cam
 
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
 
 app = Flask(__name__)
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
-@app.route('/')
+@app.route('/pic/<string:address>', methods= ["GET"])
+def here(address=None):
+    return send_from_directory(app.static_folder, 'images/123_Octavia_St.jpg', mimetype='image/jpg')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """Video streaming home page."""
+    if request.method == 'POST':
+            if request.form.get('up') == 'up':
+                # pass
+                # print("UP UP")
+                return render_template('up.html')
+            elif  request.form.get('down') == 'down':
+                # pass # do something else
+                # print("DOWN DOWN")
+                return render_template('down.html')
+            else:
+                # pass # unknown
+                return render_template('index.html')
     return render_template('index.html')
 
 
@@ -37,4 +68,6 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    my_ip = get_ip()
+    sys.stderr.write(app.instance_path)
+    app.run(host=my_ip, threaded=True, debug=True)
